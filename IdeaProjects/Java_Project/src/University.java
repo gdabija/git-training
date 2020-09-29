9bc974e21e0fbcdfabc3ef7f67658c5db1c13ddb
Grigore Dabija <Grigore.Dabija@endava.com>
2020-09-29 11:40:33 +0300

    Java project

---
+++ IdeaProjects/Java_Project/src/University.java
@@ -0,0 +1,11 @@
+public class University {
+    private String name;
+    private String rectorsName;
+    private int age;
+
+    public University (String name, String rectorsName, int age){
+        this.age=age;
+        this.name = name;
+        this.rectorsName = rectorsName;
+    }
+}
